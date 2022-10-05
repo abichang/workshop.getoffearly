1f48c711de813cd3a3a167362a6bb956b44722b9
Abi Chang <abi@cloud-latitude.com>
2022-10-05 14:05:07 +0800

    first commit

---
+++ src/main/java/com/cloudlatitude/workshop/getoffearly/GetOffEarlyApplication.java
@@ -0,0 +1,13 @@
+package com.cloudlatitude.workshop.getoffearly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetOffEarlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetOffEarlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudlatitude/workshop/getoffearly/GetOffEarlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudlatitude.workshop.getoffearly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetOffEarlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
