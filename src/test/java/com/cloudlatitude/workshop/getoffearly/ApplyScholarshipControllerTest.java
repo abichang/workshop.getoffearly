f9cc80dfe2dd049c2e10fbb0f7018dfc8d1947a5
Abi Chang <abi@cloud-latitude.com>
2022-10-08 11:16:14 +0800

    add controller

--- src/test/java/com/cloudlatitude/workshop/getoffearly/ApplyScholarshipControllerTest.java
+++ src/test/java/com/cloudlatitude/workshop/getoffearly/ApplyScholarshipControllerTest.java
@@ -1,4 +1,10 @@
 package com.cloudlatitude.workshop.getoffearly;
 
+import org.junit.jupiter.api.Test;
+
 public class ApplyScholarshipControllerTest {
+    @Test
+    void all_ok() throws Exception {
+        new ApplyScholarshipController();
+    }
 }
